45a789ac8417cb26daab01e566bc23590640046d
Augusto <gutopssouza@gmail.com>
2019-07-29 21:02:50 -0300

    :rocket: Implement component PostItem to finish the task

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
 
+import PostItem from "./PostItem";
+import profile1 from "../assets/perfil.jpg";
+import profile2 from "../assets/ramos.jpg";
+import profile3 from "../assets/zozo.jpg";
+import profile4 from "../assets/je.jpg";
+import profile5 from "../assets/de.jpg";
+
 class PostList extends Component {
   state = {
     posts: [
@@ -7,11 +14,59 @@ class PostList extends Component {
         id: 1,
         author: {
           name: "Augusto Pacheco",
-          avatar: ""
+          avatar: profile1
         },
         date: "29 Jul 2019",
         content: "Primeiro post desafio 04 bootcamp",
-        comments: [{}]
+        comments: [
+          {
+            id: 2,
+            author: {
+              name: "Doutor Ramos",
+              avatar: profile2
+            },
+            date: "29 Jul 2019",
+            content: "Queijo sem queijo com queijo"
+          },
+          {
+            id: 3,
+            author: {
+              name: "ZOZO",
+              avatar: profile3
+            },
+            date: "29 Jul 2019",
+            content: "Vou dominar o mundo"
+          }
+        ]
+      },
+      {
+        id: 4,
+        author: {
+          name: "Augusto Pacheco",
+          avatar: profile1
+        },
+        date: "29 Jul 2019",
+        content: "Segundo post desafio 04 bootcamp",
+        comments: [
+          {
+            id: 5,
+            author: {
+              name: "Jecota",
+              avatar: profile4
+            },
+            date: "29 Jul 2019",
+            content: "Bestão"
+          },
+          {
+            id: 6,
+            author: {
+              name: "De linda",
+              avatar: profile5
+            },
+            date: "29 Jul 2019",
+            content: "Amo vc"
+          }
+        ]
       }
     ]
   };
@@ -20,9 +75,7 @@ class PostList extends Component {
     return (
       <div className="postlist">
         {this.state.posts.map(post => (
-          <p key={post.id} className="post-content">
-            {post.content}
-          </p>
+          <PostItem key={post.id} {...post} />
         ))}
       </div>
     );
